0426c2ceb8aeffa37370405b46228881b6f81539
AlexSemak <semakaleksandr2014@gmail.com>
2025-11-04 11:20:51 +0200

    eslint improvements

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import neostandard from "neostandard";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsparser from "@typescript-eslint/parser";
+import promisePlugin from "eslint-plugin-promise";
 
 export default [
   // Apply neostandard config to all files
@@ -19,6 +20,7 @@ export default [
     },
     plugins: {
       "@typescript-eslint": tseslint,
+      promise: promisePlugin,
     },
     rules: {
       // 🎨 Override neostandard stylistic rules to match Prettier settings
@@ -47,12 +49,37 @@ export default [
       ],
       "@stylistic/indent": "off", // Turn off - let Prettier handle indentation
 
-      // ⚠️ IMPORTANT: These rules catch missing await keywords
-      "@typescript-eslint/no-floating-promises": "error", // Requires promises to be handled
-      "@typescript-eslint/no-misused-promises": "error", // Prevents misuse of promises
+      // ⚠️ IMPORTANT: These rules catch missing await keywords and Promise handling
+      "@typescript-eslint/no-floating-promises": [
+        "error",
+        {
+          ignoreVoid: true,
+          ignoreIIFE: false,
+        },
+      ], // Requires promises to be handled
+      "@typescript-eslint/no-misused-promises": [
+        "error",
+        {
+          checksVoidReturn: true,
+          checksConditionals: true,
+          checksSpreads: true,
+        },
+      ], // Prevents misuse of promises in conditions and spreads
       "@typescript-eslint/await-thenable": "error", // Ensures await is only used on promises
+      "@typescript-eslint/promise-function-async": "warn", // Functions returning promises should be async
+      "@typescript-eslint/no-confusing-void-expression": [
+        "error",
+        {
+          ignoreArrowShorthand: true,
+        },
+      ], // Prevents confusing void expressions
       "require-await": "off", // Turn off base rule
       "@typescript-eslint/require-await": "off", // Turn off - Fastify plugins often don't need await
+
+      // Promise plugin rules
+      "promise/always-return": "off", // Too strict for our use case
+      "promise/catch-or-return": "off", // We use async/await
+      "promise/valid-params": "error", // Validates Promise constructor params
 
       // Additional helpful rules
       "@typescript-eslint/no-unused-vars": [
@@ -62,6 +89,11 @@ export default [
           varsIgnorePattern: "^_",
         },
       ],
+
+      // This will catch usage of Promise values as regular values
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
     },
   },
 
